bf0f534c9ca8a307f3b284121da201dcca579157
Nano-AI <adi.bankoti@gmail.com>
2020-09-19 20:30:36 -0700

    Updated code

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Nitro Hack
-Hacks for [Nitro Type](https://www.nitrotype.com/). Max WPM is over 1500.
+# Typing Websites Hack
+Hacks for one website.
+- [Nitro Type](https://www.nitrotype.com/).
+- [TypeRacer](https://play.typeracer.com/).
+
+> :warning: The cheats might not work. Check the warnings tab in this file.
+
+## Installation
+Run `pip install -r requirements.txt` in the directory to install all requirements.
 
 ## Usage
-Run `pip install -r requirements.txt` in the directory to install all requirements.
 Hit enter in the Terminal to launch a new race, hit enter in the terminal again, once the text
 box loads in, hit enter in the Terminal again. Once the race starts, hit enter in the typing box to start the hack.
+
+## :warning: Warnings
---
+++ TypeRacer.py
@@ -0,0 +1,39 @@
+import time
+import random
+import string
+
+import keyboard as kb
+from pynput.keyboard import Controller, Key
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
+while True:
+    intext = input("Hit enter to get ready. (Or type p and hit enter for pass)")
+    if intext != "p":
+        driver.get('https://play.typeracer.com/')
+    input("Hit enter once page is loaded")
+    sleep = float(input('Enter time sleep'))
+    sleepHalf = sleep / 2
+    words = driver.find_elements_by_css_selector('table.inputPanel')
+    # print(words[0].text)
+    print(f"Press enter to start")
+    time.sleep(1)
+    typeKey = Controller()
+    while not kb.is_pressed('enter'):
+        time.sleep(0.01)
+    for word in words:
+        print(word.text)
+        for character in word.text:
+            if random.randint(0, 10) <= 2:
+                randKey = random.choice(string.ascii_letters)
+                typeKey.press(randKey)
+                typeKey.release(randKey)
+                time.sleep(sleepHalf)
+                typeKey.press(Key.backspace)
+                typeKey.release(Key.backspace)
+            typeKey.press(character)
+            typeKey.release(character)
+            time.sleep(sleep)
